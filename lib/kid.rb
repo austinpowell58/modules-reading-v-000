e71921500dabe75a34b3f2fdc40d918ac88dc669
Austin Powell <austinpowell58@gmail.com>
2018-08-23 17:58:16 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,5 +3,6 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
